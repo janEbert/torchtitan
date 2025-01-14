edaee563d7f6165ce1e623acfde5ab5807e819ea
janEbert <janpublicebert@posteo.net>
2025-01-14 15:52:41 +0100

    Re-add Llama-2 model
    Since it shared the architecture with Llama-3 before, we just add a new
    Llama-3 flavor for it.

--- torchtitan/models/llama/__init__.py
+++ torchtitan/models/llama/__init__.py
@@ -10,6 +10,7 @@ from torchtitan.components.loss import cross_entropy_loss
 from torchtitan.components.lr_scheduler import build_lr_schedulers
 from torchtitan.components.optimizer import build_optimizers
 from torchtitan.datasets.hf_datasets import build_hf_dataloader
+from torchtitan.datasets.tokenizer.sentencepiece import build_sentencepiece_tokenizer
 from torchtitan.datasets.tokenizer.tiktoken import build_tiktoken_tokenizer
 from torchtitan.protocols.train_spec import register_train_spec, TrainSpec
 
@@ -59,6 +60,15 @@ llama3_configs = {
     ),
 }
 
+llama2_configs = {
+    "debugmodel": llama3_configs["debugmodel"],
+    "7B": TransformerModelArgs(
+        dim=4096,
+        n_layers=32,
+        n_heads=32,
+    ),
+}
+
 
 register_train_spec(
     TrainSpec(
@@ -71,6 +81,21 @@ register_train_spec(
         build_lr_schedulers_fn=build_lr_schedulers,
         build_dataloader_fn=build_hf_dataloader,
         build_tokenizer_fn=build_tiktoken_tokenizer,
+        loss_fn=cross_entropy_loss,
+    )
+)
+
+register_train_spec(
+    TrainSpec(
+        name="llama2",
+        cls=Transformer,
+        config=llama2_configs,
+        parallelize_fn=parallelize_llama,
+        pipelining_fn=pipeline_llama,
+        build_optimizers_fn=build_optimizers,
+        build_lr_schedulers_fn=build_lr_schedulers,
+        build_dataloader_fn=build_hf_dataloader,
+        build_tokenizer_fn=build_sentencepiece_tokenizer,
         loss_fn=cross_entropy_loss,
     )
 )
