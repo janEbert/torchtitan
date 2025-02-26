3d74096d186dff5956dee9fbd11c438e7ee06bc3
janEbert <janpublicebert@posteo.net>
2025-02-26 10:05:15 +0100

    Try to fix argparsing

--- torchtitan/config_manager.py
+++ torchtitan/config_manager.py
@@ -319,7 +319,7 @@ class JobConfig:
         self.parser.add_argument(
             "--training.dataset_files",
             nargs='*',
-            type=str,
+            type=string_list,
             help="Dataset files to use (only necessary for certain types of datasets)",
         )
         self.parser.add_argument(
