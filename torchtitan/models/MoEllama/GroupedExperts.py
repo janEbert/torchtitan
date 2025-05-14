43b8180fb35112d871f88b3abeee045b805f1a9b
rakkit <wang55@jwlogin24.juwels>
2025-05-14 02:53:37 +0200

    fix init_all_experts_different

--- torchtitan/models/MoEllama/GroupedExperts.py
+++ torchtitan/models/MoEllama/GroupedExperts.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 
 from . import ep_comm
 from torchtitan.models.inits import build_init_fn
+import torch.distributed as dist
+import random
 
 
 class GroupedExperts(nn.Module):
@@ -22,7 +24,7 @@ class GroupedExperts(nn.Module):
 
     ep_mesh = None
     ep_size = 1
-    ep_rank = None
+    ep_local_rank = None
     expert_per_rank = None
 
     def __init__(
@@ -60,7 +62,7 @@ class GroupedExperts(nn.Module):
     def setup_ep(self, ep_mesh, ep_size):
         self.ep_mesh = ep_mesh
         self.ep_size = ep_size
-        self.ep_rank = ep_mesh.get_local_rank()
+        self.ep_local_rank = ep_mesh.get_local_rank()
         self.expert_per_rank = self.num_experts // ep_size
 
     def forward(
@@ -99,16 +101,14 @@ class GroupedExperts(nn.Module):
         gate_init_std = init_std / residual_div if init_gate_as_residual else init_std
 
         if self.init_all_experts_same:
-            init_all_experts_same(init_fn, self.gate_proj.data, init_std)
-            init_all_experts_same(init_fn, self.down_proj.data, gate_init_std)
-            init_all_experts_same(init_fn, self.up_proj.data, init_std / residual_div)
-        else:
-            if init_fn_type != "scaled_orthogonal":
-                raise ValueError(f"Unsupported init_fn_type: {init_fn_type}")
+            expert_init_fn = init_all_experts_same
 
-            init_all_experts_different(self, self.gate_proj.data, init_std)
-            init_all_experts_different(self, self.down_proj.data, gate_init_std)
-            init_all_experts_different(self, self.up_proj.data, init_std / residual_div)
+        else:
+            expert_init_fn = init_all_experts_different
+
+        expert_init_fn(init_fn, self.gate_proj.data, init_std)
+        expert_init_fn(init_fn, self.down_proj.data, gate_init_std)
+        expert_init_fn(init_fn, self.up_proj.data, init_std / residual_div)
 
 
 def init_all_experts_same(init_fn, w, init_std):
@@ -124,53 +124,28 @@ def init_all_experts_same(init_fn, w, init_std):
     if isinstance(w, torch.distributed.tensor.DTensor):
         w.to_local().data = local_tensor
     else:
-        w.copy_(local_tensor)
+        w.to_local().copy_(local_tensor)
 
 
-def init_all_experts_different(self, w, init_std):
-    """
-    here we hardcode init with the sclaed orthogonal init
-    """
-    fan_out, fan_in = w.shape[1], w.shape[2]
-    gain = (fan_out / fan_in) ** 0.5
-    gain *= init_std
-    big_matrix = torch.empty([self.num_experts, fan_out, fan_in], device=w.device)
-    torch.nn.init.normal_(big_matrix, mean=0.0, std=1)
-
-    if not isinstance(w, torch.distributed.tensor.DTensor):
-        dtensor_matrix = big_matrix
-        # there is DDP
-
-    elif self.ep_mesh is not None and self.ep_size > 1:
-        # for experts parallel
-        start_idx = self.ep_rank * self.expert_per_rank
-        end_idx = start_idx + self.expert_per_rank
-        dtensor_matrix = big_matrix[start_idx:end_idx, :, :]
+def init_all_experts_different(init_fn, w, init_std):
+    if isinstance(w, torch.distributed.tensor.DTensor):
+        local_tensor = w.to_local()
     else:
-        # for fsdp + no experts parallel
-        dtensor_matrix = torch.distributed.tensor.distribute_tensor(
-            big_matrix, placements=w.placements, device_mesh=w.device_mesh
-        ).to_local()
+        local_tensor = w
 
-    # now we do orthogonal init on the dtensor_matrix
-    for e in range(dtensor_matrix.shape[0]):
-        data = dtensor_matrix[e]
-        if fan_out < fan_in:
-            data.t_()
+    for e in range(local_tensor.shape[0]):
+        rank = dist.get_rank()
+        seed = rank * 50000 + e * 100 + random.randint(0, 10000)
+        # for each rank, layer, expert, [w1, w2, w3], we need to set a different seed
+        if w.device.type == "meta":
+            rng = None
+        else:
+            rng = torch.Generator(device=w.device)
+            rng.manual_seed(seed)
 
-        q, r = torch.linalg.qr(data)
-        d = torch.diag(r, 0)
-        ph = d.sign()
-        q *= ph
-
-        if fan_out < fan_in:
-            q.t_()
-        with torch.no_grad():
-            data.view_as(q).copy_(q)
-            data.mul_(gain)
-            dtensor_matrix[e].data = data
+        init_fn(local_tensor[e], mean=0.0, std=init_std, generator=rng)
 
     if isinstance(w, torch.distributed.tensor.DTensor):
-        w.to_local().data.copy_(dtensor_matrix)
+        w.to_local().copy_(local_tensor)
     else:
-        w.copy_(dtensor_matrix)
+        w.copy_(local_tensor)
